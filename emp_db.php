895525c2e3c7d8616a7a4f7f91ce253db837d806
Abdul Bari <142833264+abdulbariburiro@users.noreply.github.com>
2025-10-21 18:01:12 +0500

    Initial commit

---
+++ emp_db.php
@@ -0,0 +1,11 @@
+<?php
+$servername = '127.0.0.1';
+$username = 'root';
+$password = 'root';
+
+$conn = new mysqli($servername, $username, $password);
+
+if($conn->connect_error){
+    die("Connection Failed" . $conn->connect_error);
+}
+?>
